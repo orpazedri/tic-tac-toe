478163d4b0adcab9f3ae39c6dfe5fb7c836097bb
Orpaz Edri <orpaz258258@gmail.com>
2025-07-30 01:02:21 +0300

    shits

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,15 +6,16 @@ const initialGameBoard = [
     [null, null, null],
 ]
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
     const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
 function handleSelecetSquare(rowIndex, colIndex) {
     setGameBoard((prevGameBoard) => {
         const updateBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-        updateBoard[rowIndex][colIndex] = 'X';
+        updateBoard[rowIndex][colIndex] = activePlayerSymbol;
         return updateBoard;
     });
+    onSelectSquare()
 }
 
 
